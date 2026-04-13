c45332d5a10de700e1a219c5e9fedfd0d846bf43
Vinh <lephuvinh110104@gmail.com>
2026-04-14 06:04:55 +0700

    update new file to store video

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,27 +1,27 @@
 export const artifactVideos = [
   {
     id: "CV001",
-    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+    videoUrl: "/videos/CV001.mp4",
     title: "Video Thuyết minh: Trống Đồng Đông Sơn"
   },
   {
     id: "CV002",
-    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+    videoUrl: "/videos/CV002.mp4",
     title: "Video Thuyết minh: Bình Gốm Chu Đậu"
   },
   {
     id: "CV003",
-    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+    videoUrl: "/videos/CV003.mp4",
     title: "Video Thuyết minh: Ấn Vàng Hoàng Đế Chi Bảo"
   },
   {
     id: "CV004",
-    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+    videoUrl: "/videos/CV004.mp4",
     title: "Video Thuyết minh: Tượng Thần Shiva"
   },
   {
     id: "CV005",
-    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+    videoUrl: "/videos/CV005.mp4",
     title: "Video Thuyết minh: Cọc Gỗ Bạch Đằng"
   }
 ];
